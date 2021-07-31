fa7b2e428618466c68b072b889957ae073fbf30b
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-07-31 17:40:02 +0800

    Update QuadTree.h

--- nbody_BH_serial/Header/QuadTree.h
+++ nbody_BH_serial/Header/QuadTree.h
@@ -2,16 +2,37 @@
 #define QuadTree_H_
 
 #include "Body.h"
+#include "Quad.h";
 #include <vector>
 #include <SFML/Graphics.hpp>
 
 class QuadTree {
 public:
-    QuadTree(int left, int top, int width, int height);
-    bool InsertElement(Body* b);
-    void Subdivide(QuadTree* root);
-    int GetSize() const {
-        return children_.size();
+    QuadTree();
+    QuadTree(Quad* boundary);
+    void InsertElement(Body* b, float _zoom);
+    //void updateMXY(Body* b);
+    void addBody(Body* b, float _zoom);
+
+    //Calculate Force
+    void UpdateForce(Body* b, float _zoom);
+
+    bool CheckInternal() const {
+        return (GetNW() == NULL && GetNE() == NULL && GetSW() == NULL && GetSE() == NULL);
+    }
+
+    bool CheckExternal(QuadTree t) const {
+        if (t.GetNW() == NULL && t.GetNE() == NULL && t.GetSW() == NULL && t.GetSE() == NULL)
+            return true;
+
+        return false;
+    }
+
+    Body* GetBody() const {
+        return body_;
+    }
+    Quad* GetBoundary() const {
+        return boundary_;
     }
     QuadTree* GetNW() const {
         return NW_;
@@ -25,23 +46,26 @@ public:
     QuadTree* GetSE() const {
         return SE_;
     }
+    bool GetDivided() const {
+        return divided_;
+    }
     sf::IntRect GetRect() const {
         return rect_;
     }
     sf::RectangleShape GetRectShape() const {
         return rect_shape_;
     }
-    std::vector<Body*> GetChildren() const {
-        return children_;
-    }
-    void QtreeCheckCollisions(int& num_collisions);
+    //void QtreeCheckCollisions(int& num_collisions);
     void QtreeFreeMemory();
 private:
-    std::vector<Body*> children_;
+    const double Theta = 0.5;
+    Body* body_;
+    Quad* boundary_;
     QuadTree* NW_ = nullptr;
     QuadTree* NE_ = nullptr;
     QuadTree* SW_ = nullptr;
     QuadTree* SE_ = nullptr;
+    bool divided_ = false;
     sf::RectangleShape rect_shape_;
     sf::IntRect rect_;
 };
